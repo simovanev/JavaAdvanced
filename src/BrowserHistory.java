64aa62afdc00b600e4b0dc2311cef0d3cc392bb5
simovanev <simovanev@abv.bg>
2023-01-12 05:35:44 +0200

    Stacks and Queues

--- src/BrowserHistory.java
+++ src/BrowserHistory.java
@@ -1,2 +1,26 @@
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
 public class BrowserHistory {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String address = scanner.nextLine();
+        ArrayDeque<String> history = new ArrayDeque<>();
+        while (!address.equals("Home")) {
+            if (address.equals("back")) {
+                String currentURL="";
+                if (history.size() <= 1) {
+                    System.out.println("no previous URLs");
+                } else currentURL=history.pop();
+
+            } else {
+                history.push(address);
+                System.out.println(history.peek());
+            }
+
+
+            address = scanner.nextLine();
+        }
+
+    }
 }
