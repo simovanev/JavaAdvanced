f73f4246a92945c50a1732b5724b9fe69d59e6ef
simovanev <simovanev@abv.bg>
2023-01-13 03:59:07 +0200

    Stacks and Queues

--- src/StacksAndQueues/PrinterQueue.java
+++ src/StacksAndQueues/PrinterQueue.java
@@ -1,4 +1,26 @@
 package StacksAndQueues;
 
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
 public class PrinterQueue {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        String docName= scanner.nextLine();
+        ArrayDeque<String> printerQueue=new ArrayDeque<>();
+        while (!docName.equals("print")){
+            if (docName.equals("cancel")){
+                if (printerQueue.isEmpty()){
+                    System.out.println("Printer is on standby");
+                    docName= scanner.nextLine();
+                    continue;
+                }else System.out.println("Canceled "+printerQueue.poll());
+            }else printerQueue.offer(docName);
+
+            docName= scanner.nextLine();
+        }
+        while (!printerQueue.isEmpty()){
+            System.out.println(printerQueue.poll());
+        }
+    }
 }
