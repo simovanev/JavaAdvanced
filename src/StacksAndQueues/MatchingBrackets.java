9a1485f0fc87dad60d9881ff21523dbf514b6477
simovanev <simovanev@abv.bg>
2023-01-13 03:46:08 +0200

    Stacks and Queues

---
+++ src/StacksAndQueues/MatchingBrackets.java
@@ -0,0 +1,21 @@
+package StacksAndQueues;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class MatchingBrackets {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        String input= scanner.nextLine();
+        ArrayDeque<Integer> index=new ArrayDeque<>();
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i)=='('){
+                index.push(i);
+            } else if (input.charAt(i)==')') {
+                int starIndex= index.pop();
+                int endIndex = i+1;
+                System.out.println(input.substring(starIndex,endIndex));
+            }
+        }
+    }
+}
---
+++ src/StacksAndQueues/PrinterQueue.java
@@ -0,0 +1,4 @@
+package StacksAndQueues;
+
+public class PrinterQueue {
+}
