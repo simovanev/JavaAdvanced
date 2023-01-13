dad07c843d46d4ef608e54d0bcaaa469501c5c7b
simovanev <simovanev@abv.bg>
2023-01-13 04:20:59 +0200

    Stacks and Queues

--- src/StacksAndQueues/HotPotato.java
+++ src/StacksAndQueues/HotPotato.java
@@ -1,4 +1,24 @@
 package StacksAndQueues;
 
+import java.util.ArrayDeque;
+import java.util.Collection;
+import java.util.List;
+import java.util.Scanner;
+
 public class HotPotato {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        String[] kids=scanner.nextLine().split("\\s+");
+        int passes= Integer.parseInt(scanner.nextLine());
+        ArrayDeque <String> kidsInTheGame=new ArrayDeque<>();
+        kidsInTheGame.addAll(List.of(kids));
+        while (kidsInTheGame.size()>1){
+            for (int i = 1; i <passes ; i++) {
+                String rotation=kidsInTheGame.poll();
+                kidsInTheGame.offer(rotation);
+            }
+            System.out.println("Removed "+kidsInTheGame.poll());
+        }
+        System.out.println("Last is "+kidsInTheGame.poll());
+    }
 }
