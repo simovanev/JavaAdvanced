33c8a6bf51b1202eb73f202440efce82e17e3b66
simovanev <simovanev@abv.bg>
2023-01-14 09:57:56 +0200

    Stacks and Queues

---
+++ src/StacksAndQueues/BasicStackOperations.java
@@ -0,0 +1,30 @@
+package StacksAndQueues;
+
+import java.util.ArrayDeque;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class BasicStackOperations {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int n= scanner.nextInt();
+        int s= scanner.nextInt();
+        int x= scanner.nextInt();
+        ArrayDeque<Integer> stack=new ArrayDeque<>();
+        for (int i = 0; i < n; i++) {
+            int currentNum= scanner.nextInt();
+            stack.push(currentNum);
+        }
+        for (int i = 0; i < s; i++) {
+            stack.pop();
+        }
+        if (stack.isEmpty()){
+            System.out.println(0);
+            return;
+        }
+        if (stack.contains(x)){
+            System.out.println("true");
+        }else System.out.println(Collections.min(stack));
+
+    }
+}
