7dfc45501b31ed3ed4ffc1019ab80a235d1f3c7b
simovanev <simovanev@abv.bg>
2023-01-14 10:13:22 +0200

    Stacks and Queues

--- src/StacksAndQueues/MaximumElement.java
+++ src/StacksAndQueues/MaximumElement.java
@@ -1,4 +1,23 @@
 package StacksAndQueues;
 
+import java.util.ArrayDeque;
+import java.util.Collections;
+import java.util.Scanner;
+
 public class MaximumElement {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int n= scanner.nextInt();
+        ArrayDeque<Integer> sequence=new ArrayDeque<>();
+        for (int i = 0; i <= n; i++) {
+            String command= scanner.nextLine();
+            if (command.startsWith("1")){
+                sequence.push(Integer.parseInt(command.split("\\s+")[1]));
+            } else if (command.equals("2")) {
+                sequence.pop();
+            } else if (command.equals("3")) {
+                System.out.println(Collections.max(sequence));
+            }
+        }
+    }
 }
