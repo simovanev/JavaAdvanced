b1c03f8d224ae083df2ee003a87cf76eef99c632
simovanev <simovanev@abv.bg>
2023-01-18 05:34:23 +0200

    Multidimensional Arrays

--- src/MultidimensionalArrays/MatrixShuffling.java
+++ src/MultidimensionalArrays/MatrixShuffling.java
@@ -4,7 +4,38 @@ import java.util.Scanner;
 
 public class MatrixShuffling {
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+        int rows = scanner.nextInt();
+        int columns = scanner.nextInt();
+        String[][] matrix = new String[rows][columns];
+        for (int row = 0; row < rows; row++) {
+            matrix[row] = scanner.nextLine().split(" \\s+");
+        }
+        String[] command = scanner.nextLine().split("\\s+");
+        if (CommandIsValid(command,matrix)){
+            //todo switch
+        }
 
+    }
+
+    private static boolean CommandIsValid(String[] command, String [][] matrix) {
+
+        if (command.length != 5) {
+            return false;
+        }
+        int row1=Integer.parseInt(command[1]);
+        int col1=Integer.parseInt(command[2]);
+        int row2=Integer.parseInt(command[3]);
+        int col2=Integer.parseInt(command[4]);
+        if (!command[0].equals("swap")) {
+            return false;
+        }
+        else if (row1<0 || row2<0 ||col1<0 || col2<0 ||
+                row1>matrix.length-1 || row2> matrix.length-1 ||
+                col1>matrix[0].length-1 || col2>matrix[0].length-1)  {
+            return false;
+
+        }
+        return true;
     }
 }
