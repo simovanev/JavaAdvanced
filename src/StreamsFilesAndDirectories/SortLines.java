e03c321858db34afa69957daf66cc49b2a08ee04
simovanev <simovanev@abv.bg>
2023-01-23 16:42:45 +0200

    Streams,Files And Directories

--- src/StreamsFilesAndDirectories/SortLines.java
+++ src/StreamsFilesAndDirectories/SortLines.java
@@ -1,4 +1,24 @@
 package StreamsFilesAndDirectories;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
 public class SortLines {
+    public static void main(String[] args) throws IOException {
+        String pathIn = "C:\\Users\\20161004\\Desktop\\Java\\JavaAdvancedLabAnd Exersises\\StreamsFilesAndDirectories\\04. Java-Advanced-Files-and-Streams-Lab-Resources\\input.txt";
+        String pathOut = "C:\\Users\\20161004\\Desktop\\Java\\JavaAdvancedLabAnd Exersises\\StreamsFilesAndDirectories\\04. Java-Advanced-Files-and-Streams-Lab-Resources\\06.SortLinesOutput.txt";
+        Scanner scanner=new Scanner(new FileInputStream(pathIn));
+        PrintWriter print= new PrintWriter(new FileOutputStream(pathOut));
+        List<String> lines= Files.readAllLines(Path.of(pathIn));
+        lines=lines.stream().filter(l->!l.isBlank()).collect(Collectors.toList());
+        Collections.sort(lines);
+        Files.write(Path.of(pathOut),lines);
+        print.close();
+
+    }
 }
