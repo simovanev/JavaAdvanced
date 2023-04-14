85afa705b17b7f8b658b8b87dc5dff8bfa6aa622
simovanev <simovanev@abv.bg>
2023-04-14 12:27:32 +0300

    Exam Preparation

---
+++ src/ExamPreparation/EnergyDrinks.java
@@ -0,0 +1,37 @@
+package ExamPreparation;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+
+public class EnergyDrinks {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Deque<Integer> caffeineStack = new ArrayDeque<>();
+        Deque<Integer> energyDrinkQueue = new ArrayDeque<>();
+        String[] caffeine = scanner.nextLine().split(" ");
+        String[] energyDrink = scanner.nextLine().split(" ");
+        int StamatTotalCaffeine = 0;
+        for (String input : caffeine) {
+            caffeineStack.push(Integer.parseInt(input));
+        }
+        for (String input : energyDrink) {
+            energyDrinkQueue.offer(Integer.parseInt(input));
+        }
+        while (caffeineStack.size() > 0 || energyDrinkQueue.size() > 0) {
+            int currentCaffeine = 0;
+
+            currentCaffeine += caffeineStack.peek() + energyDrinkQueue.peek();
+
+            if (StamatTotalCaffeine + currentCaffeine <= 300) {
+                StamatTotalCaffeine += currentCaffeine;
+                caffeineStack.pop();
+                energyDrinkQueue.poll();
+            }else if (StamatTotalCaffeine + currentCaffeine > 300){
+                caffeineStack.pop();
+                energyDrinkQueue.push(energyDrinkQueue.poll());
+                StamatTotalCaffeine-=30;
+            }
+        }
+    }
+}
